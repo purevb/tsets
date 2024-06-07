812bbaadabd67f7e1ad97b9067e972c1c27a2237
purevb <purevbaatar221@gmail.com>
2024-06-07 14:42:43 +0800

    first commit

---
+++ lib/widgets/movie_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MovieCard extends StatelessWidget{
+  const MovieCard({super.key});
+
+  @override
+  Widget build (BuildContext context){
+    return Container();
+  }
+}
---
+++ lib/widgets/movie_special_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MovieSpecialCard extends StatelessWidget{
+  const MovieSpecialCard({super.key});
+
+  @override
+  Widget build (BuildContext context){
+    return Container();
+  }
+}
